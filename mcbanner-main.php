54069cb3b1ca4a825f81785c9071f1dabde8175b
1LiterZinalco <42828197+1LiterZinalco@users.noreply.github.com>
2022-01-19 20:48:12 +0100

    Cleanup, Translation

--- mcbanner-main.php
+++ mcbanner-main.php
@@ -1,57 +1,55 @@
 <?php
 /*
  @wordpress-plugin
- Plugin Name: Serverbanner - NameMc
- Author: Domisiding
- Author URI: https://domisiding.de
- Description: Serverbanner einbindung von NameMc durch Shortcodes
- Version: 1.0
+ Plugin Name: NameMC Serverbanner
+ Author: 1LiterZinalco, Domisiding
+ Author URI: https://github.com/1LiterZinalco/WP-Shortcode-NameMC-Serverbanner
+ Description: NameMC's server banner as a shortcode.
+ Version: 2.0
  Text Domain: mcbanner
  License: GNU General Public License v2
  Domain Path: /languages
- */
-//////////////////////////// Schutz //////////////////////////
+*/
+
 if ( ! defined( 'WPINC' ) ) {
     die;
 }
-//////////////////////////// Include and Action //////////////////////////
+
+// Include and Action
 add_action('init', 'mcbanner_shortcodes_init');
-add_action('mcbanner_language', 'mcbanner_sprache');
-//////////////////////////// Sprachen //////////////////////////
-function mcbanner_sprache() {
+add_action('mcbanner_language', 'mcbanner_language');
+
+// Language
+function mcbanner_language() {
     load_plugin_textdomain( 'mcbanner', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
-//////////////////////////// Beschreibung //////////////////////////
-array( __('Minecraft Serverbanner - NameMc', 'mcbanner'), __('Serverbanner einbindung von NameMc durch Shortcodes.', 'mcbanner') );
-//////////////////////////// Shortcodes //////////////////////////
+// Desciption
+array( __('NameMC Serverbanner', 'mcbanner'), __('NameMC\'s server banner as a shortcode.', 'mcbanner') );
+
+// Shortcode
 function mcbanner($atts = [], $content = null, $tag = '') {
-    
     $atts = array_change_key_case((array)$atts, CASE_LOWER);
     
     $skin_atts = shortcode_atts([
-        'server' => 'beispiel.de',
-        'width' => '728',
+        'server' => 'example.com',
+        'width' => '720',
         'height' => '90',
     ], $atts, $tag);
     
-    $ausgabe = '';
-    
-    $ausgabe .= '<div class="serverbanner">';
-    
-    $ausgabe .= '<iframe style="width:'. htmlspecialchars($skin_atts['width']) . 'px;height:'. htmlspecialchars($skin_atts['height']) . 'px;max-width:100%;border:none;display:block;margin:auto" src="https://de.namemc.com/server/'. htmlspecialchars($skin_atts['server']) . '/embed" width="'. htmlspecialchars($skin_atts['width']) . '" height="'. htmlspecialchars($skin_atts['height']) . '"></iframe>';
+    $output = '';
+    $output .= '<div class="serverbanner">';
+    $output .= '<iframe style="width:'. htmlspecialchars($skin_atts['width']) . 'px;height:'. htmlspecialchars($skin_atts['height']) . 'px;max-width:100%;border:none;display:block;margin:auto" src="https://en.namemc.com/server/'. htmlspecialchars($skin_atts['server']) . '/embed" width="'. htmlspecialchars($skin_atts['width']) . '" height="'. htmlspecialchars($skin_atts['height']) . '"></iframe>';
     
     if (!is_null($content)) {
-        $ausgabe .= apply_filters('the_content', $content);
-        $ausgabe .= do_shortcode($content);
+        $output .= apply_filters('the_content', $content);
+        $output .= do_shortcode($content);
     }
     
-    $ausgabe .= '</div>';
-    
-    return $ausgabe;
-    
+    $output .= '</div>';
+    return $output;
 }
-//////////////////////////// Init //////////////////////////
-function mcbanner_shortcodes_init()
-{
+
+// Init
+function mcbanner_shortcodes_init() {
     add_shortcode('mcbanner', 'mcbanner');
 }
